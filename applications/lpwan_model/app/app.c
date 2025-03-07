6bf017769b701fcbbfee14b470a6461e923d313d
dmochkas <dmytro.ochkas@imt-atlantique.fr>
2025-03-07 15:32:30 +0100

    Create template for LPWAN schc model scenario

---
+++ applications/lpwan_model/app/app.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("Demo SCHC app works!");
+}
--- config.cmake
+++ config.cmake
@@ -1,3 +1,7 @@
+# Overwrites the default config parameters from SCHC SDK
+
+set(APP_NAME "lpwan_model")
+
 set(FRAGMENTATION_API "p2p")
 set(TEMPLATE_ID "ipv4udp")
 set(TRACE_ENABLED ON)
