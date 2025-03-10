3ef6c558da8065611fff6a88f35416f87ca5a2b2
dmochkas <dmytro.ochkas@imt-atlantique.fr>
2025-03-10 10:49:55 +0100

    Enable custom template support
    - Add custom template configuration

--- config.cmake
+++ config.cmake
@@ -2,6 +2,10 @@
 
 set(APP_NAME "lpwan_model")
 
+set(TOOLCHAIN "gcc-native")
+set(TARGET "default")
 set(FRAGMENTATION_API "p2p")
-set(TEMPLATE_ID "ipv4udp")
+set(EXTENSION_API "template")
+set(TEMPLATE_ID "custom")
+set(TEMPLATE_FILE "${CMAKE_SOURCE_DIR}/templates/custom_template.c")
 set(TRACE_ENABLED ON)
